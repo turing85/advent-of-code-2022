fd44d73bb47b7a8826cc7dce6fb33bb209a3b71e
Marco Bungart <marco.bungart@googlemail.com>
2022-12-18 13:27:56 +0100

    Added test go get 100% coverage globally

---
+++ day2/src/test/java/de/turing85/advent/of/code/tttt/day/two/model/GameSelectionTest.java
@@ -0,0 +1,27 @@
+package de.turing85.advent.of.code.tttt.day.two.model;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.Random;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Game Selection test")
+class GameSelectionTest {
+  @Test
+  @DisplayName("own and opponent selection are as expected")
+  void ownAndOpponentSelectionAreAsExpected() {
+    // GIVEN
+    Random random = new Random();
+    Selection[] selectionValues = Selection.values();
+    Selection expectedOwn = selectionValues[random.nextInt(selectionValues.length)];
+    Selection expectedOpponent = selectionValues[random.nextInt(selectionValues.length)];
+
+    // WHEN
+    GameSelection actual = new GameSelection(expectedOwn, expectedOpponent);
+
+    // THEN
+    assertThat(actual.ownSelection()).isEqualTo(expectedOwn);
+    assertThat(actual.opponentSelection()).isEqualTo(expectedOpponent);
+  }
+}
