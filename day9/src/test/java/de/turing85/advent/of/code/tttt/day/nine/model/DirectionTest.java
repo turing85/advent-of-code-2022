166ed29f6831e59f9f7f2465ece42751db2fc64c
Marco Bungart <marco.bungart@googlemail.com>
2022-12-19 15:58:42 +0100

    added solution for day 9 challenge

---
+++ day9/src/test/java/de/turing85/advent/of/code/tttt/day/nine/model/DirectionTest.java
@@ -0,0 +1,23 @@
+package de.turing85.advent.of.code.tttt.day.nine.model;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+@DisplayName("Direction tests")
+class DirectionTest {
+  @ParameterizedTest(name = "char {0} is mapped to Direction.{1}")
+  @DisplayName("from char conversion works on legal input")
+  @CsvSource({"U,UP", "D,DOWN", "R,RIGHT", "L,LEFT"})
+  void worksOnLegalInput(char c, Direction expected) {
+    // GIVEN
+
+    // WHEN
+    Direction actual = Direction.of(c);
+
+    // THEN
+    assertThat(actual).isEqualTo(expected);
+  }
+}
